295d47248e512a187ae53fcdb817dec23532c3ad
José Gabriel Abreu Moreira <83605515+JGabriel-AbreuM@users.noreply.github.com>
2021-11-07 20:46:07 -0300

    Update views.py

--- DjangoNationalGeographic/Animal/views.py
+++ DjangoNationalGeographic/Animal/views.py
@@ -1,2 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import View
+import requests
+
+def home(request):
+    context = {}
+    response = requests.get('http://127.0.0.1:8000/api/animal/')
+    animals = response.json()
+
+    context["animals"] = animals
+    
+    return render(request, 'home.html', context)
+    
