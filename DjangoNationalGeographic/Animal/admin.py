b993c0275f61641561e3dc96609c66813c82c489
José Gabriel Abreu Moreira <83605515+JGabriel-AbreuM@users.noreply.github.com>
2021-11-07 20:44:37 -0300

    Update admin.py

--- DjangoNationalGeographic/Animal/admin.py
+++ DjangoNationalGeographic/Animal/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Animal
+from .models import Animal, Raca, NomeCientifico, Origem, NomePopular
 
 admin.site.register(Animal)
+admin.site.register(Raca)
+admin.site.register(Origem)
+admin.site.register(NomeCientifico)
+admin.site.register(NomePopular)
