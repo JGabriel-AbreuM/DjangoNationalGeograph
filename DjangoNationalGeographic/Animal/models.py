9c9ec670d879a32cff2cb510e54ea2999343f715
José Gabriel Abreu Moreira <83605515+JGabriel-AbreuM@users.noreply.github.com>
2021-11-07 20:45:07 -0300

    Update models.py

--- DjangoNationalGeographic/Animal/models.py
+++ DjangoNationalGeographic/Animal/models.py
@@ -2,11 +2,36 @@ from django.db import models
 from django.db.models.base import Model
 from Protecao.models import Protecao
 
-class Animal(models.Model):
+class Raca(models.Model):
     raca = models.CharField(max_length=255, null=False, blank=False)
+    
+    def __str__(self) -> str:
+        return self.raca
+
+class Origem(models.Model):
     origem = models.CharField(max_length=255, null=False, blank=False)
+    
+    def __str__(self) -> str:
+        return self.origem
+
+class NomePopular(models.Model):
     nome_popular = models.CharField(max_length=255, null=False, blank=False)
+
+    def __str__(self) -> str:
+        return self.nome_popular
+
+class NomeCientifico(models.Model):
     nome_cientifico = models.CharField(max_length=255, null=False, blank=False)
+
+    def __str__(self) -> str:
+        return self.nome_cientifico
+    
+class Animal(models.Model):
+    raca = models.ForeignKey(Raca, on_delete=models.CASCADE)
+    origem = models.ForeignKey(Origem, on_delete=models.CASCADE)
+    nome_popular = models.ForeignKey(NomePopular, on_delete=models.CASCADE)
+    nome_cientifico = models.ForeignKey(NomeCientifico, on_delete=models.CASCADE)
+    foto = models.ImageField(upload_to="imagens/")
     protecao = models.ForeignKey(Protecao, on_delete=models.CASCADE)
 
     def __str__(self):
