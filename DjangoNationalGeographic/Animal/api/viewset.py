e3b2e4a29d8192030ce3b2311565dfe2d37c7afb
José Gabriel Abreu Moreira <83605515+JGabriel-AbreuM@users.noreply.github.com>
2021-11-07 20:44:06 -0300

    Update viewset.py

--- DjangoNationalGeographic/Animal/api/viewset.py
+++ DjangoNationalGeographic/Animal/api/viewset.py
@@ -3,33 +3,12 @@ from rest_framework.serializers import Serializer
 from Animal.models import Animal
 from .serializers import AnimalSerializer
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
 
 class AnimalViewSet(ModelViewSet):
-    # queryset = Animal.objects.all()
+    queryset = Animal.objects.all()
     serializer_class = AnimalSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['nome_popular', 'raca', 'origem', 'nome_cientifico']
 
-    def get_queryset(self):
-        nome_popular = self.request.query_params.get('nome_popular', None)
-        raca = self.request.query_params.get('raca', None)
-        origem = self.request.query_params.get('origem', None)
-        nome_cientifico = self.request.query_params.get('cientifico', None)
-        queryset = Animal.objects.all()
-        presente = False
-
-        if nome_popular:
-            presente = nome_popular.lower() in "gatos" 
-        
-        if presente:
-            queryset = Animal.objects.filter(nome_popular__iexact=nome_popular)
-        
-            if raca:
-                queryset.filter(raca__iexact=raca)
-
-            if origem:
-                queryset.filter(origem__iexact=origem)
-            
-            if nome_cientifico:
-                queryset.filter(nome_cientifico__iexact=nome_cientifico)
-
-        return queryset
     
