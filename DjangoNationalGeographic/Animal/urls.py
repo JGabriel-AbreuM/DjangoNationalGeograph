e172f5cb5674cd0e43be7305a6232635750d85a7
José Gabriel Abreu Moreira <83605515+JGabriel-AbreuM@users.noreply.github.com>
2021-11-07 20:45:40 -0300

    Update urls.py

--- DjangoNationalGeographic/Animal/urls.py
+++ DjangoNationalGeographic/Animal/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from .views import home
 
 app_name = "Animal"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', home)
 ]
